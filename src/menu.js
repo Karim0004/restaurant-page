a3ae76412fe143781bc24ee4636439b12bf31a05
Karim <karimx099@gmail.com>
2022-06-19 20:02:18 +0200

    add menu items

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,27 @@
 import menuBackground from './assets/menu.jpg';
+
+import spaghetti from './assets/spaghetti.jpg';
+import burger from './assets/burger.jpg';
+import kebab from './assets/kebab.jpg';
+import salad from './assets/salad.jpg';
+
 export default function (container) {
     const menuContainer = document.createElement('div');
 
-    for (let n = 0; n<100; n ++) {
-        const item = document.createElement('div');
-        menuContainer.appendChild(item);
+    const items = [
+        {name: 'Spagheti', src: spaghetti},
+        {name: 'Burger', src: burger},
+        {name: 'Kebab', src: kebab},
+        {name: 'Salad', src: salad},
+    ]
+    for (let item of items) {
+        const itemElement = document.createElement('div');
+        itemElement.style.backgroundImage = `url(${item.src})`;
+        const text = document.createElement('p');
+        text.innerText = item.name;
+        itemElement.appendChild(text);
+        
+        menuContainer.appendChild(itemElement);
     }
 
     container.innerHTML = '';
